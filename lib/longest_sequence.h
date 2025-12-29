95ad12e2ecb0a32870d8156756179bc939b1db4f
v-vasilyev <v.vasilyev@samsung.com>
2025-12-29 14:57:59 +0300

    chore: the longest sequence example
    Signed-off-by: v-vasilyev <v.vasilyev@samsung.com>

---
+++ lib/longest_sequence.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <unordered_set>
+#include <vector>
+
+// [0, 3, 10, 4, 7, 2, 11, 9, 12] ==> {0} {2,3,4} {7} {9,10,11,12}
+/*
+    
+*/
+class XL{
+public:
+    int test(std::vector<int> nums){
+        int max_len = 0;
+    }
+};
